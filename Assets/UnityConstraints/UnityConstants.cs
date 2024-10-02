89cd7a43d2b35a0a3b623faa7d064fb7813b2f9e
Tigran Minasyan <tigran.minasyan.v@gmail.com>
2024-10-02 20:24:31 +0400

    Added enemy prefabs
    - added enamy layer
    - added untility to generate constants

---
+++ Assets/UnityConstraints/UnityConstants.cs
@@ -0,0 +1,65 @@
+// This file is auto-generated. Modifications are not saved.
+
+public static class Tags
+{
+    public const string Untagged = "Untagged";
+    public const string Respawn = "Respawn";
+    public const string Finish = "Finish";
+    public const string EditorOnly = "EditorOnly";
+    public const string MainCamera = "MainCamera";
+    public const string Player = "Player";
+    public const string GameController = "GameController";
+}
+
+public static class SortingLayers
+{
+    public const int Default = 0;
+}
+
+public static class Layers
+{
+    public const int Default = 0;
+    public const int TransparentFX = 1;
+    public const int Ignore_Raycast = 2;
+    public const int Water = 4;
+    public const int UI = 5;
+    public const int Enemy = 6;
+
+    public const int DefaultMask = 1 << 0;
+    public const int TransparentFXMask = 1 << 1;
+    public const int Ignore_RaycastMask = 1 << 2;
+    public const int WaterMask = 1 << 4;
+    public const int UIMask = 1 << 5;
+    public const int EnemyMask = 1 << 6;
+}
+
+public static class Scenes
+{
+    public const int GameScene = 0;
+}
+
+public static class Axes
+{
+    public const string Horizontal = "Horizontal";
+    public const string Vertical = "Vertical";
+    public const string Fire1 = "Fire1";
+    public const string Fire2 = "Fire2";
+    public const string Fire3 = "Fire3";
+    public const string Jump = "Jump";
+    public const string Mouse_X = "Mouse X";
+    public const string Mouse_Y = "Mouse Y";
+    public const string Mouse_ScrollWheel = "Mouse ScrollWheel";
+    public const string Submit = "Submit";
+    public const string Cancel = "Cancel";
+    public const string Enable_Debug_Button_1 = "Enable Debug Button 1";
+    public const string Enable_Debug_Button_2 = "Enable Debug Button 2";
+    public const string Debug_Reset = "Debug Reset";
+    public const string Debug_Next = "Debug Next";
+    public const string Debug_Previous = "Debug Previous";
+    public const string Debug_Validate = "Debug Validate";
+    public const string Debug_Persistent = "Debug Persistent";
+    public const string Debug_Multiplier = "Debug Multiplier";
+    public const string Debug_Horizontal = "Debug Horizontal";
+    public const string Debug_Vertical = "Debug Vertical";
+}
+
